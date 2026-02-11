45e68fcc2a7f8a3693194ec2118f03e6a836b5f5
Theos-the-dawg <motheo13@outlook.com>
2026-02-11 21:46:51 +0200

    express-initial

--- gerald.js
+++ gerald.js
@@ -1,1 +1,13 @@
-console.log("")
+const express = require('express')// importing express
+const app = express() // initialization of express 
+const port = 3000 // port 
+
+// CRUD Functions are important 
+//GET 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+// creation of server on localhost 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
