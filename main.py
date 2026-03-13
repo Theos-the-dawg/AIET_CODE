ade7e659ceb722391a559c176d62e624c4a61cfb
Theos-the-dawg <motheo13@outlook.com>
2026-03-13 11:14:03 +0200

    TASK2

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 #EXCERCISE 1  WRITE A PROGRAM THE CHECK THAT INDEX 0,1 ARE EQUAL TO EACH OTHER
+array  = [1,2,2,3,4,6]
+
+value_1 = array[0]
+value_2 = array[1]
+if value_1 == value_2:
+    print(True)
+else:
+    print(False)
+#print(value_1 == value_2)
+
+#EXCERCISE 2  WRITE A PROGRAM THE CHECK THAT INDEX 0,1 ARE EQUAL OR GREATER THAN EACH OTHER
+array  = [1,2,2,3,4,6]
