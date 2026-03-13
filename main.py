6401c940dd1d47263a0c2f1e42ec7e2a83ae5222
Theos-the-dawg <motheo13@outlook.com>
2026-03-13 14:01:06 +0200

    Homework and lessons

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
-#EXCERCISE 1  WRITE A PROGRAM THE CHECK THAT INDEX 0,1 ARE EQUAL TO EACH OTHER
-array  = [1,2,2,3,4,6]
+# # #EXCERCISE 1  WRITE A PROGRAM THE CHECK THAT INDEX 0,1 ARE EQUAL TO EACH OTHER
+# # array  = [1,2,2,3,4,6]
 
-value_1 = array[0]
-value_2 = array[1]
-if value_1 == value_2:
-    print(True)
-else:
-    print(False)
-#print(value_1 == value_2)
+# # value_1 = array[0]
+# # value_2 = array[1]
+# # if value_1 == value_2:
+# #     print(True)
+# # else:
+# #     print(False)
+# # #print(value_1 == value_2)
 
-#EXCERCISE 2  WRITE A PROGRAM THE CHECK THAT INDEX 0,1 ARE EQUAL OR GREATER THAN EACH OTHER
-array  = [1,2,2,3,4,6]
-index_1 = array[0]
-index_2 = array[1]
-if index_1 <= index_2:
-    print (True)
-else:
-    print (False)
+# # #EXCERCISE 2  WRITE A PROGRAM THE CHECK THAT INDEX 0,1 ARE EQUAL OR GREATER THAN EACH OTHER
+# # array  = [1,2,2,3,4,6]
+# # index_1 = array[0]
+# # index_2 = array[1]
+# # if index_1 <= index_2:
+# #     print (True)
+# # else:
+# #     print (False)
+
+# age = range(20,25)
+# if age is 20  != 25:
+#     pass
+# not {!=}
+# and {&}
+# is {==}
+# or 
