56293d727e543f3aa2cba59e405e307bbae95db4
Theos-the-dawg <motheo13@outlook.com>
2026-03-24 10:46:37 +0200

    COMMIT TODAYS EXCERCISE

--- main.py
+++ main.py
@@ -1,27 +1,8 @@
-#1.1 CREATE AN ARRAY AND APPEND 3 VALUES
+#TASK  1
+#CREATE A WHILE LOOP THAT CONTINUES TO RUN UNTI THE UNSER INSERTS THE LETTER 'q' IN THE INPUT
 
-array = [2,3,5,7,1,2,9,0,214,10,23]
+# TASK 2 
+# CREATE A DICTIONARY AND LOOP THROUGH THE KEYS IF A USER GETS A KEY THAT MATCHS A RANGE DISPLAY THE KEY
 
-array.append(50)
-array.append(20)
-array.append(10)
-
-print(array)
-array.sort()
-print(array)
-#1.2 FIND THE LARGEST ELEMENT IN THE ARRAY
-maximum=max(array)
-print(maximum)
-#1.3 FIND THE SMALLEST ELEMENT IN THE ARRAY 
-minimum = min(array)
-print(minimum)
-
-
-#1.4 FIND MINUS THE MAX - MIN OF THE ARRAY
-print(maximum-minimum)
-value = 0 
-for number in array:
-   value+=number
-   print(value)
-
-print(f"the avg is : {value/len(array)}") 
+# #TASK 3 
+# CREATE A LOOP THROUGH THE ARRAY AND MULTIPY THE NUMBER IN THE ARRAY BY THEMSELVES
