fb774398e82cbf088279a3b484f8d597a59d6d3b
valencycodecussler <geraldkapurura07@gmail.com>
2026-03-27 09:58:12 +0200

    latest-main

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 #TASK  1
-#CREATE A WHILE LOOP THAT CONTINUES TO RUN UNTI THE UNSER INSERTS THE LETTER 'q' IN THE INPUT
+#CREATE A WHILE LOOP THAT CONTINUES TO RUN UNTI THE UNSER INSERTS THE LETTER 'q' IN THE 
 
-# TASK 2 
-# CREATE A DICTIONARY AND LOOP THROUGH THE KEYS IF A USER GETS A KEY THAT MATCHS A RANGE DISPLAY THE KEY
 
-# #TASK 3 
-# CREATE A LOOP THROUGH THE ARRAY AND MULTIPY THE NUMBER IN THE ARRAY BY THEMSELVES
+
+checker = False
+while checker ==False:
+    user_input = input("hey input something")
+    if user_input == 'q':
+     print(f"You entered {user_input}")
+     checker=True
+    else:
+       print(f"you have entered {user_input} try again")
+
+
+# # TASK 2 
+# # CREATE A DICTIONARY AND LOOP THROUGH THE KEYS IF A USER GETS A KEY THAT MATCHS A RANGE DISPLAY THE KEY
+# my_dict = {
+#     "name" : "james"
+#     "course" : "compsci"
+#     "college" : "AIET"
+# }
+# # #TASK 3 
+# # CREATE A LOOP THROUGH THE ARRAY AND MULTIPY THE NUMBER IN THE ARRAY BY THEMSELVES``
+
+# my_array = [,1,2,3,,6,8,90,5,4,]
+# for my_array
+                    
